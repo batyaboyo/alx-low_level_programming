d42c00ff1c42902b83e9ded1a67d8991ac5dc13e
Batya Tonny <batztonnie@gmail.com>
2022-04-08 10:08:00 +0300

    More memory management in C lang

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,91 +1,107 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
-*/
-
-
-void _puts(char *str)
-{
-int i = 0;
-while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-
-}
-
-/**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
+ * is_digit - checks for digit
+ *
+ * @s: string operand
+ *
+ * Return: 0 or 1
  */
 
-int _atoi(const char *s)
+int is_digit(char *s)
 {
-    int sign = 1;
-	unsigned long int resp = 0, firstNum, i;
+	int i = 0;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	while (s[i])
 	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
+}
 
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+/**
+ * _strlen - string length
+ *
+ * @s: string operand
+ *
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		resp *= 10;
-		resp += (s[i] - 48);
+		i++;
 	}
-
-	return (sign * resp);
+	return (i);
 }
 
 /**
- * print_int - prints an integer.
- * @n: int
- * Return: 0
+ * errors - handles errors for main
  */
 
-void print_int(unsigned long int n)
+void errors(void)
 {
-
-unsigned  long int divisor = 1, i, resp;
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-}
-
-}
-
-/**
- * main - print the result of the multiplication, followed by a new line
- * @argc: int
- * @argv: list
- * Return: 0
- */
-
-int main(int argc, char const *argv[])
-{
-(void)argc;
-
-if (argc != 3)
-{
-	_puts("Error ");
+	printf("Error\n");
 	exit(98);
 }
-print_int(_atoi(argv[1]) * _atoi(argv[2]));
-_putchar('\n');
 
-return (0);
+/**
+ * main - multiplies two positive numbers
+ *
+ * @argc: argument counter
+ *
+ * @argv: argument vector
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int l1, l2, l, i, n, digit1, digit2, *ptr, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	l = l1 + l2 + 1;
+	ptr = malloc(sizeof(int) * l);
+	if (!ptr)
+		return (1);
+	for (i = 0; i <= l1 + l2; i++)
+		ptr[i] = 0;
+	for (l1 = l1 - 1; l1 >= 0; l1--)
+	{
+		digit1 = s1[l1] - '0';
+		n = 0;
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			digit2 = s2[l2] - '0';
+			n += ptr[l1 + l2 + 1] + (digit1 * digit2);
+			ptr[l1 + l2 + 1] = n % 10;
+			n /= 10;
+		}
+		if (n > 0)
+			ptr[l1 + l2 + 1] += n;
+	}
+	for (i = 0; i < l - 1; i++)
+	{
+		if (ptr[i])
+			a = 1;
+		if (a)
+			_putchar(ptr[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(ptr);
+	return (0);
 }
